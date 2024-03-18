3a67b117aabcdfa8dff39d5e37d06fdbff4d6cb4
Timothy Carl Bundalian <90750916+timbundalian@users.noreply.github.com>
2024-03-18 20:59:54 +0800

    updatesOusrStory

--- src/pages/OurStory.tsx
+++ src/pages/OurStory.tsx
@@ -2,10 +2,105 @@ import Header from "../components/Header";
 
 const OurStory = () => {
   return (
-    <div>
-      <h1 className=" text-4xl">Welcome to the Our Story page!</h1>
+    <div className="px-10">
+      <div className="p-10 justify-center" >
+      <h1 className=" text-4xl font-bold text-white text-center ">Our Story</h1>
       {/* Add your other components and content here */}
     </div>
+    <div className="divide-y-8 divide-yellow-400 px-10 text-white">
+      
+      <div className="p-20 flex justify-center">
+        <div>
+          <p className="indent-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tortor nisl, dictum id pulvinar ut, fringilla
+          bibendum elit. Morbi auctor lectus erat,
+          a interdum magna lobortis vitae. Donec sapien purus, mattis id sollicitudin in, dapibus volutpat elit. Quisque sed dignissim urna. Interdum et malesuada 
+          fames ac ante ipsum primis in faucibus. Suspendisse blandit pharetra sapien at fringilla. Vivamus sed auctor justo, vel congue orci. Donec vehicula
+          congue libero at condimentum. Donec tincidunt arcu non laoreet varius. Nunc hendrerit risus quis vestibulum tempor. Donec eleifend, quam ut rutrum 
+          porttitor, urna nibh iaculis leo, eu viverra justo velit vel dolor. Maecenas sed fermentum quam. Curabitur tempor vitae sapien euismod egestas.Aenean 
+          ut scelerisque sapien. Curabitur posuere mauris eu felis vehicula tempor. Proin tempus leo mi, in rutrum leo euismod in.</p>
+        </div>
+        <div className="h-20 bg-yellow border-l-2 border-yellow"></div>
+      </div>
+      <div className="flex justify-center mx-auto p-10">
+        <div className="mx-auto flex justify-center flex flex-col space-y-1">
+          <h1 className="text-2xl font-bold">History</h1><br/>
+          <div className="flex rounded-sm w-96 h-16 p-10 hover:shadow-lg">
+            <h1 className="text-white font">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt vel magnam quaerat debitis, autem sequi aperiam voluptatum assumenda eaque reprehenderit, quo impedit voluptatem eos laborum, temporibus natus molestiae dolores beatae.  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt vel magnam quaerat debitis, autem sequi aperiam voluptatum assumenda eaque reprehenderit, quo impedit voluptatem eos laborum, temporibus natus molestiae dolores beatae.  </h1>
+          </div>
+          <div className="mx-auto flex justify-center flex flex-col space-y-4">
+          <div className="flex rounded-sm w-96 h-24 bg-white shadow p-20 hover:shadow-lg">
+          </div>
+        </div>    
+        </div> 
+        <div className="mx-auto flex justify-center flex flex-col space-y-4">
+          <div className="flex rounded-sm w-96 h-24 bg-white shadow p-20 hover:shadow-lg">
+          </div>
+          <div className="flex rounded-sm w-96 h-16 p-10 hover:shadow-lg">
+            <h1 className="text-white font">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt vel magnam quaerat debitis, autem sequi aperiam voluptatum assumenda eaque reprehenderit, quo impedit voluptatem eos laborum, temporibus natus molestiae dolores beatae.  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt vel magnam quaerat debitis, autem sequi aperiam voluptatum assumenda eaque reprehenderit, quo impedit voluptatem eos laborum, temporibus natus molestiae dolores beatae.  </h1>
+          </div>
+        </div>      
+      </div>
+      <div className="flex justify-center mx-auto p-10">
+        <div className="mx-auto flex justify-center flex flex-col">
+          <h1 className="text-2xl font-bold text-center">Mission</h1><br/>
+          <div className="flex rounded-sm h-16 p-10 hover:shadow-lg">
+            <h1 className="text-white font">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt vel magnam quaerat debitis, autem sequi aperiam voluptatum assumenda eaque reprehenderit, quo impedit voluptatem eos laborum, temporibus natus molestiae dolores beatae.  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt vel magnam quaerat debitis, autem sequi aperiam voluptatum assumenda eaque reprehenderit, quo impedit voluptatem eos laborum, temporibus natus molestiae dolores beatae.  </h1>
+          </div>    
+        </div> 
+        <div className="mx-auto flex justify-center flex flex-col space-y-4">
+        <h1 className="text-2xl font-bold  text-center">Vission</h1><br/>
+          <div className="flex rounded-sm  h-16 p-10 hover:shadow-lg">
+            <h1 className="text-white font">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt vel magnam quaerat debitis, autem sequi aperiam voluptatum assumenda eaque reprehenderit, quo impedit voluptatem eos laborum, temporibus natus molestiae dolores beatae.  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt vel magnam quaerat debitis, autem sequi aperiam voluptatum assumenda eaque reprehenderit, quo impedit voluptatem eos laborum, temporibus natus molestiae dolores beatae.  </h1>
+          </div>
+        </div>      
+      </div>
+      
+      <div className="p-20 flex justify-center flex-col">
+        <div className="pb-8">
+          <h1 className="text-2xl font-bold">Bachelor of Arts in Broadcasting</h1><br />
+          
+        </div>
+        
+        <div className=" h-96 justify-center flex flex-col space-y-4">
+          <div className="flex rounded-sm  size-full h-96  bg-white shadow  hover:shadow-lg">
+            
+          </div>
+          
+        </div>
+         
+      </div>
+      <div className="h-20 bg-yellow border-l-2 border-yellow"></div>
+
+      <div className="flex justify-center mx-auto p-10">
+       
+          <h1 className="text-2xl font-bold">Management Team</h1><br/>
+          <div className="mx-auto  justify-center flex flex-col space-y-4">
+          <div className="flex rounded-sm  h-24 bg-white shadow p-20 hover:shadow-lg"></div>
+          <h1 className="text-center">Name</h1>
+          <div className="mx-auto  justify-center flex flex-col space-y-4">
+          <div className="flex rounded-sm  h-24 bg-white shadow p-20 hover:shadow-lg"></div>
+          <h1 className="text-center">Name</h1>
+        </div>      
+        </div>    
+        <div className="mx-auto  justify-center flex flex-col space-y-4">
+        <div className="flex rounded-sm  h-24 bg-white shadow p-20 hover:shadow-lg"></div>
+        <h1 className="text-center">Name</h1>
+        <div className="mx-auto  justify-center flex flex-col space-y-4">
+        <div className="flex rounded-sm  h-24 bg-white shadow p-20 hover:shadow-lg"></div>
+        <h1 className="text-center">Name</h1>
+        </div>      
+        </div>      
+        <div className="mx-auto justify-center flex flex-col space-y-4">
+        <div className="flex rounded-sm  h-24 bg-white shadow p-20 hover:shadow-lg"></div>
+        <h1 className="text-center">Name</h1>
+        <div className="mx-auto  justify-center flex flex-col space-y-4">
+        <div className="flex rounded-sm  h-24 bg-white shadow p-20 hover:shadow-lg"></div>
+        <h1 className="text-center">Name</h1>
+        </div>      
+        </div>      
+      </div>
+    </div>
+  </div> 
   );
 };
 
